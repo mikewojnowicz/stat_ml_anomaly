a95c80a1e19b6383e6ae9d7a6829c1e23059e85f
Mike Wojnowicz <mwojnowi@uci.edu>
2020-11-09 07:26:12 -0800

    _Actually_ added code/ to ignore/

---
+++ ignore/code/tests/integration/test_integration.py
@@ -0,0 +1,6 @@
+import pytest  # noqa
+
+def test_model_accuracy():
+    # integration tests verify behavior of the project that also depends on
+    # the behavior of other projects or services
+    pass
